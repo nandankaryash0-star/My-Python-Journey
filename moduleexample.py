3aebbdde56035d504d09bef788a9b73d492807f4
nandankaryash0-star <nandankaryash0@gmail.com>
2025-11-28 22:31:51 +0530

    status updated

--- moduleexample.py
+++ moduleexample.py
@@ -1,1 +1,13 @@
 pi = 3.14
+
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def cercumference(radius):
+    return 2 * pi * radius
+
+def area(radius):
+    return pi * radius ** 2
---
+++ modules.py
@@ -0,0 +1,17 @@
+#import math as m
+
+#from math import e
+#a,b,c,d = 1,2,3,4
+
+#print(e ** a)
+#print(e ** b)
+#print(e ** c)
+#print(e ** d)
+
+import moduleexample
+x = int(input("Enter the number: "))
+result = moduleexample.square(x)
+result = moduleexample.cube(x)
+result = moduleexample.cercumference(x)
+
+print(result)
